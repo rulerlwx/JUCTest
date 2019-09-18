06dd8e8ff70f8a7da58bf9583a01c7ee91474909
black_wolf <leafsing20309@163.com>
2019-09-19 00:51:01 +0800

    不安全的集合，分析、解决方案

--- src/main/java/com/demo/juc/ContainerNotSafeTest.java
+++ src/main/java/com/demo/juc/ContainerNotSafeTest.java
@@ -1,12 +1,14 @@
 package com.demo.juc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * 集合不安全
+ */
 public class ContainerNotSafeTest {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<String> list = new CopyOnWriteArrayList<>();
         for (int i = 0; i < 3; i++) {
             new Thread(()->{
                 list.add(UUID.randomUUID().toString().substring(0,8));
@@ -19,7 +21,8 @@ public class ContainerNotSafeTest {
          * 2 导致原因
          *
          * 3 解决方案
-         *
+         *  3.1 new Vector<>();
+         *  3.2 Collections.synchronizedList(new ArrayList<>());
          * 4 优化建议（同样的错误不犯第2次）
          *
          */
